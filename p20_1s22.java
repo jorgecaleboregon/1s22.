ccccd53353ea9a61f3daa04a065f6c15b8d8ac48
jorgecaleboregon <144292860+jorgecaleboregon@users.noreply.github.com>
2023-10-03 17:31:15 -0600

    Add files via upload

--- p20_1s22.java
+++ p20_1s22.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class p20_1s22{
+    public static void main(String[] args) {
+        java.util.Scanner teclado = new Scanner(System.in);
+        double radio;
+        double area;
+        double longitud;
+        System.out.println("calcular el area y la longitud");
 
-	public static void main(String[] args) {
-		
-		java.util.Scanner teclado = new Scanner (System.in);
-double radio;
-double area;
-double longitud;
-System.out.println(" calcula el area y longitud");
-System.out.println("Escribe un radio:");
-radio= teclado.nextInt();
-area = Math.PI*Math.pow(radio,2);
-// area = PI*r^2
-longitud = 2*Math.PI*radio;
-System.out.println("el resultado del area es: "+ longitud);
-
-	}
+        System.out.println("escribe un radio: ");
+        radio = teclado.nextInt();
+        area = Math.PI*Math.pow(radio,2);
+       
+        longitud = 2*Math.PI*radio;
+        System.out.println("el resultado del area es: " + area);
+        System.out.println("el resultado de longitud es: " + longitud);
+       
+    }
 }
