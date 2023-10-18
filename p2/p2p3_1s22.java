1e4be0f30d6bf2263f604d83648c2ab4514ca35e
jorgecaleboregon <144292860+jorgecaleboregon@users.noreply.github.com>
2023-10-17 19:27:21 -0600

    Add files via upload

---
+++ p2/p2p3_1s22.java
@@ -0,0 +1,21 @@
+public class p2p3_1s22{
+	public static void main(String[] args) {
+		 int a=0;
+
+		while(a<+10){
+			System.out.println("el valor de a vale" +a);
+			a++;
+		}
+		a=0;
+		boolean estado = true;
+			System.out.println("Nuevamente a vale "+a);
+			if (a>10) break;
+			a++;
+		}
+		 estado = false;
+			System.out.println("se mostrara esta linea?");
+		}
+	
+
+
+
